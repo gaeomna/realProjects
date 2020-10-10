fff34521174e3e4bcab14a2a74e6b08a32ba5a42
gaeomna <gaeomna@naver.com>
2020-10-11 01:17:28 +0900

    넘김

--- src/main/java/com/wakuza/springboot/realProjects/modules/settings/SettingsController.java
+++ src/main/java/com/wakuza/springboot/realProjects/modules/settings/SettingsController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.validation.Valid;
 import java.nio.channels.AcceptPendingException;
 
